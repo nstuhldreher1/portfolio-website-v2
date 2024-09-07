f7aa8a837ee4517db1bfddf9c31984bf028ad01c
nstuhldreher1 <nbstuhldreher@gmail.com>
2024-09-07 15:57:52 -0400

    feat: boilerplate for website

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import HomePage from './pages/HomePage';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="container">
+      <div  className= "item">
+        <HomePage/>
+      </div>
+    </div>
+  )
+}
+
+export default App
