0860f46f42be5f3dcb2e8da417d3344e46228f94
nstuhldreher1 <nbstuhldreher@gmail.com>
2024-09-07 16:33:47 -0400

    feat: setup grid

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
 
   return (
-    <div className="container">
-      <div  className= "item">
+    <div className="grid grid-cols-5 pt-16">
+      <div  className= "col-start-2 col-end-5">
         <HomePage/>
       </div>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ function HomePage(){
 
     return (
         <div>
-            <h1>Hello World!</h1>
+            <h1 className="font-mono">Hello World!</h1>
         </div>
     );
 
